7d4ae289501ca5fc756030c5603051153df1b8d1
Raj Shah <rajshah11112003@gmail.com>
2025-06-02 15:53:54 +0530

    feat: added database connectivity, initail setup, schema setup and config for drizzle

---
+++ config/index.ts
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Speak Easy",
+  description:
+    "Interactive platform for language learning with lessons, quizzes, and progress tracking.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "duoSpeak Easy-clone",
+    "learn-language",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "Speak Easy",
+    "postgresql",
+    "sonner",
+    "drizzle",
+    "zustand",
+    "mysql",
+    "lucide-react",
+    "next-themes",
+    "clerk-themes",
+    "clerk",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Sanidhya Kumar Verma",
+    url: "https://github.com/sanidhyy",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/sanidhyy/duoSpeak Easy-clone",
+  email: "sanidhya.verma12345@gmail.com",
+} as const;
